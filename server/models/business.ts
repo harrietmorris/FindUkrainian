98a6d3640dcf6e23083a59a4147994cbf07a4901
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-15 17:35:24 +0200

    business schema refactored to typescript

--- server/models/business.ts
+++ server/models/business.ts
@@ -1,11 +1,31 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+import {model, Schema } from 'mongoose';
 
 //FixMe: review 'require' for all if pulling data from API (not mock)
 //Resolution: No specified API to fetch from. Deciding to leave schema as is.
+interface IBusinessType {
+  _id: string;
+  name: string;
+  address: string;
+  category: string;
+  description: string;
+  longDescription: string;
+  website: string;
+  openingHours: {
+    [key: string]: OpenCloseHours;
+  };
+  contactInfo: string;
+  coordinates: { lat: number; lng: number };
+  images: [string];
+}
+
+interface OpenCloseHours {
+  [key: string]: number;
+}
 
 //FixMe: review openingHours type (Number) -> (String)
 //Resolution: Mock data is still hard coded. 
-const businessSchema = new mongoose.Schema({
+const businessSchema: Schema =  new Schema<IBusinessType>({
   name: { type: String, required: true },
   address: { type: String, required: true },
   category: { type: String, required: true },
@@ -50,6 +70,6 @@ const businessSchema = new mongoose.Schema({
   images: { type: [String], required: true },
 });
 
-const Business = mongoose.model('Business', businessSchema);
+const Business = model<IBusinessType>('Business', businessSchema);
 
 module.exports = Business;
