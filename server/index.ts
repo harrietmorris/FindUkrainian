b819d04bca4370a191f1809309734915868c932c
Regina Czech <regina.czech@yahoo.com>
2024-06-15 17:34:15 +0200

    fix: index to ts file

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router.js");
+const dotenv = require("dotenv");
 const { PORT } = require("./config.js");
 const { connectDB } = require("./models/index.js");
 
-require("dotenv").config();
+dotenv.config();
 
 const app = express();
 
@@ -12,8 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.use((_req, res) => {
-  res.status(404).send("This route does not exist");
+app.use((_req: Request, res: Response) => {
+  res.status(404)
+  res.send("This route does not exist");
 });
 
 (async () => {
