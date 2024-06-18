ff5a5ccfa571f466f1792050b0ccd97f60dfba20
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-18 17:43:36 +0200

    more testing

--- client/Tests/render.test.tsx
+++ client/Tests/render.test.tsx
@@ -1,17 +1,36 @@
-import {render,fireEvent, queryByPlaceholderText, screen} from '@testing-library/react'
+import { render, fireEvent, queryByPlaceholderText, screen } from '@testing-library/react'
 // import React from 'react'
 // import Search from '../src/components/Search/Search'
 import '@testing-library/jest-dom'
 import App from '../src/App'
 import { expect, describe, it } from 'vitest'
 import { BrowserRouter } from 'react-router-dom'
+import Map from '../src/components/Map/Map'
+import { MapContainer } from 'react-leaflet'
 
 
-
-describe('title is rendering',()=>{
-    it('title is visible', ()=>{
-        render(<App/> , {wrapper: BrowserRouter})
+describe('title is rendering', () => {
+    it('title is visible', () => {
+        render(<App />, { wrapper: BrowserRouter })
         expect(screen.getByText(/Find 🇺🇦 services in Köln 🇩🇪/i)).toBeInTheDocument()
         // screen.debug();
     })
+
+    // it('should render the map', async () => {
+    //     const { container } = render(<Map />);
+    //     const mapContainer = container.querySelector('.leaflet-container');
+    //     expect(await mapContainer).toBeInTheDocument();
+
+    // });
+
+    it('check detail button', async () => {
+        render(<Map />);
+        
+        expect(
+            await screen.findByRole("button",{name:"Details"})
+        ).toBeInTheDocument();
+
+    });
+
 })
+
