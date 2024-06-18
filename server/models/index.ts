875e820ee15bc524f625aad2001ded10dc5b505a
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-18 10:40:51 +0200

    server is working as intended

--- server/models/index.ts
+++ server/models/index.ts
@@ -11,4 +11,4 @@ export async function connectDB() {
   }
 }
 
-module.exports = { connectDB }; 
+export default { connectDB }; 
--- server/router.ts
+++ server/router.ts
@@ -1,8 +1,10 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+import Router from 'express'
 import {getAllMiddleware, getByIdMiddleware} from './controller';
 import Business from "./models/business";
 import Category from "./models/category";
 
+const router = Router()
 router.get("/businesses", getAllMiddleware(Business));
 router.get("/categories", getAllMiddleware(Category));
 router.get("/businesses/:id", getByIdMiddleware(Business));
