c7ec874846e035531d43bc3010bf148683d28c0a
Harriet <Harriet.mae.morris@gmail.com>
2024-06-18 16:47:37 +0200

    category filter test, and mocks created. also updated read me for tsx commands

---
+++ client/Tests/Mocks/mockCategories.tsx
@@ -0,0 +1,54 @@
+const mockCategories = [
+    {
+      name: {
+        en: 'all',
+        uk: 'Всі',
+      },
+      description: {
+        en: 'All services and products',
+        uk: 'Всі послуги та продукти',
+      },
+    },
+    {
+      name: {
+        en: 'Restaurants',
+        uk: 'Ресторани',
+      },
+      description: {
+        en: 'Places where you can eat',
+        uk: 'Місця, де можна поїсти',
+      },
+    },
+    {
+      name: {
+        en: 'Health',
+        uk: "Здоров'я",
+      },
+      description: {
+        en: 'Health-related services and products',
+        uk: "Послуги та продукти, пов'язані зі здоров'ям",
+      },
+    },
+    {
+      name: {
+        en: 'Beauty',
+        uk: 'Краса',
+      },
+      description: {
+        en: 'Beauty salons and services',
+        uk: 'Салони краси та послуги',
+      },
+    },
+    {
+      name: {
+        en: 'Grocery Stores',
+        uk: 'Продуктові магазини',
+      },
+      description: {
+        en: 'Stores selling food and everyday items',
+        uk: 'Магазини, що продають продукти та повсякденні товари',
+      },
+    },
+  ];
+  
+export default mockCategories;
