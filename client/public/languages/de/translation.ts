a6b1a12141e0300340bdb617b21be4bbf26473b3
Harriet <Harriet.mae.morris@gmail.com>
2024-06-19 17:30:28 +0200

    translate dropdown working for title

---
+++ client/public/languages/de/translation.ts
@@ -0,0 +1,5 @@
+export const de = {
+
+    "welcome": "Finden Sie 🇺🇦 Dienstleistungen in Köln 🇩🇪",
+    "description": "Dies ist die deutsche Übersetzung"
+  }
---
+++ client/public/languages/en/translation.ts
@@ -0,0 +1,4 @@
+export const en = {
+    "welcome": "Find 🇺🇦 services in Köln 🇩🇪",
+    "description": " Find 🇺🇦 services in Köln 🇩🇪"
+  }
